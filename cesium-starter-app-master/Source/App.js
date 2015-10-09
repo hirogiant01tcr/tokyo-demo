98c312bbfb8479e6fe37e4c9dd17dd063c3a8695
hirogiant01tcr <usui20130508@gmail.com>
2015-10-09 11:23:50 +0900

    ラインを追加

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -21,3 +21,19 @@ point2.add({
             color : Cesium.Color.BLUE
     }
 });
+
+//ƒ‰ƒCƒ“‚ð’Ç‰Á
+var line1 = viewer.entities;
+
+line1.add({
+    name : 'Red line on the surface',
+    polyline : {
+        positions : Cesium.Cartesian3.fromDegreesArray([-75, 35,
+                                                        -125, 35]),
+        width : 5,
+        material : Cesium.Color.RED
+    }
+});
+
+
+
