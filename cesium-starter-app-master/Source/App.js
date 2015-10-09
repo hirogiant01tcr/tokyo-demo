0011f563ab9e7b66585ad7831ae613fcd141487c
hirogiant01tcr <usui20130508@gmail.com>
2015-10-09 12:49:53 +0900

    kml修正3

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -11,6 +11,7 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 viewer.dataSources.add(Cesium.KmlDataSource.load('http://hirogiant01tcr.github.io/tokyo-demo/cesium-starter-app-master/Source/1009.kml'));
 
 
+
 //‹“_‚Ì•ÏX
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
 
