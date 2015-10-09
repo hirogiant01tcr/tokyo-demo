92485cca3809b822db9de656fa2df917f4f28197
hirogiant01tcr <usui20130508@gmail.com>
2015-10-09 12:58:16 +0900

    Update App.js

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -2,12 +2,12 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 
 
 
-//kml�ǂݍ���2
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://hirogiant01tcr.github.io/tokyo-demo/cesium-starter-app-master/Source/1009.kml'));
+//kml“Ç‚Ýž‚Ý2
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://raw.githubusercontent.com/hirogiant01tcr/tokyo-demo/gh-pages/cesium-starter-app-master/Source/1009.kml'));
 
 
 
-//���_�̕ύX
+//Ž‹“_‚Ì•ÏX
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
 
 
